1bffc70c6f9823f96c37f06938d40ac55a4a16fb
june <dydrkfl078@naver.com>
2024-06-04 01:26:46 +0900

    :recycle:Appconfig 리팩토링 - 역할과 구현 분리

--- prac2_core/src/main/kotlin/prac2/core/AppConfig.kt
+++ prac2_core/src/main/kotlin/prac2/core/AppConfig.kt
@@ -1,18 +1,29 @@
 package prac2.core
 
+import prac2.core.discount.DiscountPolicy
 import prac2.core.discount.FixDiscountPolicy
 import prac2.core.order.OrderService
 import prac2.core.order.OrderServiceImpl
+import prac2.core.repository.MemberRepo
 import prac2.core.repository.MemoryMemberRepo
 import prac2.core.service.MemberService
 import prac2.core.service.MemberServiceImpl
 
 object AppConfig  {
+
+    private fun memberRepo(): MemberRepo {
+        return MemoryMemberRepo()
+    }
+
+    private fun discountPolicy(): DiscountPolicy {
+        return FixDiscountPolicy()
+    }
+
     fun memberService(): MemberService {
-        return MemberServiceImpl( MemoryMemberRepo() )
+        return MemberServiceImpl( memberRepo() )
     }
 
     fun orderService(): OrderService {
-        return OrderServiceImpl( MemoryMemberRepo(),FixDiscountPolicy())
+        return OrderServiceImpl( memberRepo() , discountPolicy())
     }
 }
