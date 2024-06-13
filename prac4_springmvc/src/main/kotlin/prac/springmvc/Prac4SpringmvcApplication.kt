6a237240510e57fcbcec79f1033dd0b8192fb1e5
june <dydrkfl078@naver.com>
2024-06-13 21:26:30 +0900

    :tada:Prac4 SpringMVC 학습 시작

---
+++ prac4_springmvc/src/main/kotlin/prac/springmvc/Prac4SpringmvcApplication.kt
@@ -0,0 +1,11 @@
+package prac.springmvc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Prac4SpringmvcApplication
+
+fun main(args: Array<String>) {
+	runApplication<Prac4SpringmvcApplication>(*args)
+}
