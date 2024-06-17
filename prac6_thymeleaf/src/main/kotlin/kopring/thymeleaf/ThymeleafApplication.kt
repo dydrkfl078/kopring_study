c369ae2a07dede80e7ee016338943f6780d826e3
june <dydrkfl078@naver.com>
2024-06-18 05:23:42 +0900

    :tada:Thymeleaf 학습용  프로젝트 시작

---
+++ prac6_thymeleaf/src/main/kotlin/kopring/thymeleaf/ThymeleafApplication.kt
@@ -0,0 +1,11 @@
+package kopring.thymeleaf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThymeleafApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThymeleafApplication>(*args)
+}
---
+++ prac6_thymeleaf/src/test/kotlin/kopring/thymeleaf/ThymeleafApplicationTests.kt
@@ -0,0 +1,13 @@
+package kopring.thymeleaf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
