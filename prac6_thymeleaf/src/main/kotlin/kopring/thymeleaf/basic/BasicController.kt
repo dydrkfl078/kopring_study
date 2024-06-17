1b24868725b9761f66a6aaf76af0e29044212398
june <dydrkfl078@naver.com>
2024-06-18 08:59:39 +0900

    :books:Thymeleaf 텍스트, URL, 변수선언 학습

---
+++ prac6_thymeleaf/src/main/kotlin/kopring/thymeleaf/basic/BasicController.kt
@@ -0,0 +1,46 @@
+package kopring.thymeleaf.basic
+
+import jakarta.servlet.http.HttpSession
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/basic")
+class BasicController {
+
+    @GetMapping("text-basic")
+    fun textBasic(model : Model) :String {
+        model.addAttribute("data","Hello Thymeleaf!")
+        model.addAttribute("data2","<b>is bold?</b>")
+        return "/basic/text-basic"
+    }
+
+    @GetMapping("variable")
+    fun variable(model : Model) :String {
+        val testList = listOf(1,2,3,4,5)
+        model.addAttribute("list",testList)
+        return "/basic/variable"
+    }
+
+    @GetMapping("basic-objects")
+    fun basicObjects(session : HttpSession) :String {
+        session.setAttribute("sessionData","Hello Session!")
+
+        return "/basic/basic-objects"
+    }
+
+    @GetMapping("link")
+    fun link(): String{
+        return "/basic/link"
+    }
+}
+
+@Component
+class TestSpringBean(){
+    fun hello(string: String = ""): String {
+        return "HelloSpringBean${string}"
+    }
+}
