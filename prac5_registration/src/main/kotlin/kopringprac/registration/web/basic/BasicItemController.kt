b45c82accdaa90e33d8567b2e2b0b158520c1915
june <dydrkfl078@naver.com>
2024-06-18 04:57:31 +0900

    :books:상품 등록 프로젝트 - [메모리] 상품 CRU 추가 ( Delete는 X ) - Thymeleaf 를 통한 동적 ModelView 학습 - PRG ( Post - Redirection - Get ) 학습

---
+++ prac5_registration/src/main/kotlin/kopringprac/registration/web/basic/BasicItemController.kt
@@ -0,0 +1,85 @@
+package kopringprac.registration.web.basic
+
+import kopringprac.registration.domain.item.Item
+import kopringprac.registration.domain.item.ItemRepo
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import kotlin.reflect.jvm.internal.impl.util.MemberKindCheck.Member
+
+@Controller
+@RequestMapping("/basic/items")
+class BasicItemController(private val itemRepo: ItemRepo) {
+
+    init {
+        itemRepo.save(Item("testItemA", 50000, 30))
+        itemRepo.save(Item("testItemB", 20000, 8))
+    }
+
+    @GetMapping
+    fun items(model: Model): String {
+        model.addAttribute("items", itemRepo.findAll())
+        return "basic/items"
+    }
+
+    @GetMapping("/{itemId}")
+    fun item(@PathVariable itemId: Long, model: Model): String {
+        val findItem = itemRepo.findById(itemId)
+        model.addAttribute("item", findItem)
+        return "basic/item"
+    }
+
+    @GetMapping("add")
+    fun addForm(): String {
+        return "basic/addForm"
+    }
+
+//    @PostMapping("add")
+    fun save(
+        @RequestParam itemName: String,
+        @RequestParam price: Int,
+        @RequestParam quantity: Int,
+        model: Model
+    ): String {
+        val item = Item(itemName,price,quantity)
+        itemRepo.save(item)
+
+        model.addAttribute("item", item)
+        return "basic/item"
+    }
+
+    // @ModelAttribute 를 명시적으로 작성하여 자동으로 Item 객체를 만들어 준다.
+    // @ModelAttribute("item") 으로 명시적으로 Model.setAttribute 해줄 수 있으나, 생략할 시 자동으로 클래스 이름으로 생성.
+    // @ModelAttribute 자체도 생략이 가능하지만... 왠만하면 명시적으로 적어주는 것이 직관적이고 좋을 것 같다.
+    @PostMapping("/add")
+    fun saveV2(@ModelAttribute item : Item, rda : RedirectAttributes): String{
+
+        itemRepo.save(item)
+        rda.addAttribute("saveItem",item.id)
+        rda.addAttribute("state", true)
+
+//        model.addAttribute("item", item)
+
+        return "redirect:/basic/items/{saveItem}"
+    }
+
+    @GetMapping("/{itemId}/edit")
+    fun editForm(@PathVariable itemId : Long, model: Model): String {
+        val item = itemRepo.findById(itemId)
+
+        model.addAttribute("item", item)
+        return "basic/editForm"
+    }
+
+    @PostMapping("/{itemId}/edit")
+    fun edit(@PathVariable itemId: Long,@ModelAttribute item : Item): String {
+        itemRepo.update(item.id!!,item)
+        return "redirect:/basic/items/{itemId}"
+    }
+}
