e5e25c3525c3113d9cbad0be26ae90a411046fd2
june <dydrkfl078@naver.com>
2024-06-30 14:28:26 +0900

    :tada:Spring - file upload 학습 시작

---
+++ prac10_upload/src/main/kotlin/kopring/prac10_upload/Prac10UploadApplication.kt
@@ -0,0 +1,11 @@
+package kopring.prac10_upload
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Prac10UploadApplication
+
+fun main(args: Array<String>) {
+	runApplication<Prac10UploadApplication>(*args)
+}
---
+++ prac10_upload/src/test/kotlin/kopring/prac10_upload/Prac10UploadApplicationTests.kt
@@ -0,0 +1,13 @@
+package kopring.prac10_upload
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Prac10UploadApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
