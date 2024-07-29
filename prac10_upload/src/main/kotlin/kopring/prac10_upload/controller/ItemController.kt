1f981749632ab9da4cc1d11379c3babd86b55b37
june <dydrkfl078@naver.com>
2024-07-29 15:32:23 +0900

    :sparkles:File upload - ContentDisposition builder를 통한 파일 다운로드 학습

--- prac10_upload/src/main/kotlin/kopring/prac10_upload/controller/ItemController.kt
+++ prac10_upload/src/main/kotlin/kopring/prac10_upload/controller/ItemController.kt
@@ -1,10 +1,14 @@
 package kopring.prac10_upload.controller
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import kopring.prac10_upload.domain.Item
 import kopring.prac10_upload.domain.ItemRepo
 import kopring.prac10_upload.file.FileStore
 import org.springframework.core.io.Resource
 import org.springframework.core.io.UrlResource
+import org.springframework.http.ContentDisposition
+import org.springframework.http.HttpHeaders
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,6 +17,10 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import java.net.MalformedURLException
+import java.nio.charset.StandardCharsets
+
+private val logger = KotlinLogging.logger {  }
 
 @Controller
 class ItemController(private val fileStore: FileStore, private val itemRepo : ItemRepo) {
@@ -47,9 +55,33 @@ class ItemController(private val fileStore: FileStore, private val itemRepo : It
     }
 
     @ResponseBody
-    @GetMapping("/images/{filename}")
-    fun downloadImage(@PathVariable filename : String): Resource {
-        return UrlResource("file:${fileStore.getFullPath(filename)}")
+    @GetMapping("/images/{fileName}")
+    fun downloadImage(@PathVariable fileName : String): Resource {
+        return UrlResource("file:${fileStore.getFullPath(fileName)}")
+    }
+
+    @GetMapping("/attach/{itemId}")
+    fun downloadFile(@PathVariable itemId : Long): ResponseEntity<Resource> {
+        val item = itemRepo.findById(itemId)
+
+        item?.let {
+            val storeFileName = it.attachFile.storeFileName
+            val uploadFileName = it.attachFile.uploadFileName
+
+            val urlResource = UrlResource("file:${fileStore.getFullPath(storeFileName)}")
+
+            logger.info { "uploadFileName : $uploadFileName" }
+
+            val contentDisposition = ContentDisposition.attachment().filename(uploadFileName,StandardCharsets.UTF_8).build().toString()
+
+            logger.info { "contentDisposition : $contentDisposition" }
+
+            return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(urlResource)
+
+        }?: throw MalformedURLException()
+
     }
 
 }
