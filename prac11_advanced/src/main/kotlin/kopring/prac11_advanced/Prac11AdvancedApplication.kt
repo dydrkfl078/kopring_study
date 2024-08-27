41ac2337908a38991ad857f3b5215f0015f92e2f
june <dydrkfl078@naver.com>
2024-08-28 06:15:05 +0900

    :tada:Kopring 핵심원리 고급편 시작

---
+++ prac11_advanced/src/main/kotlin/kopring/prac11_advanced/Prac11AdvancedApplication.kt
@@ -0,0 +1,11 @@
+package kopring.prac11_advanced
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Prac11AdvancedApplication
+
+fun main(args: Array<String>) {
+	runApplication<Prac11AdvancedApplication>(*args)
+}
---
+++ prac11_advanced/src/test/kotlin/kopring/prac11_advanced/Prac11AdvancedApplicationTests.kt
@@ -0,0 +1,13 @@
+package kopring.prac11_advanced
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Prac11AdvancedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
