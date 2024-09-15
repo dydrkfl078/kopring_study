b821581aadda08729bcee5e1a55f7418361259c8
june <dydrkfl078@naver.com>
2024-09-15 14:08:43 +0900

    :zap: AOP - Pointcut signature ( around advice 와 pointcut 분리 )

--- prac12_aop/src/main/kotlin/kopring/prac12_aop/order/aop/AspectV1.kt
+++ prac12_aop/src/main/kotlin/kopring/prac12_aop/order/aop/AspectV1.kt
@@ -4,13 +4,26 @@ import org.aspectj.lang.annotation.Aspect
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Pointcut
 
 private val logger = KotlinLogging.logger {  }
 
 @Aspect
 class AspectV1 {
 
-    @Around("execution(* kopring.prac12_aop.order..*(..))")
+    // V1 - 포인트 컷을 분리하지 않는 Aspect
+//    @Around("execution(* kopring.prac12_aop.order..*(..))")
+//    fun doLog(joinPoint: ProceedingJoinPoint): Any {
+//        logger.info { "[Log] ${joinPoint.signature}" }
+//        return joinPoint.proceed() ?: Unit
+//    }
+
+    // V2 - 포인트 컷을 분리하는 Aspect
+    // 다른 곳에서도 해당 포인트 컷을 재사용할 수 있다.
+    @Pointcut("execution(* kopring.prac12_aop.order..*(..))")
+    fun allOrder() {} // pointcut signature
+
+    @Around("allOrder()")
     fun doLog(joinPoint: ProceedingJoinPoint): Any {
         logger.info { "[Log] ${joinPoint.signature}" }
         return joinPoint.proceed() ?: Unit
